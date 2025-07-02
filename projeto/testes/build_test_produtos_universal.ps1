6321a863bd7bc5e2561ff3c8a24c75e1a37a1f50
Joaquim Antonio <joaquim.antonio.camargos@gmail.com>
2025-07-01 23:40:23 -0300

    quase la

--- projeto/testes/build_test_produtos_universal.ps1
+++
@@ -1,9 +0,0 @@
-# Compilação universal para rodar de qualquer pasta
-
-$scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
-cd $scriptPath
-
-gcc ../codigo/produtos.c test_produtos.c munit/munit.c -I../codigo -Imunit -o test_produtos.exe
-
-Write-Host "Compilação finalizada. Para rodar os testes, execute:"
-Write-Host ".\test_produtos.exe"
